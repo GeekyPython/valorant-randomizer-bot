fb4c98b2ce5f02364865bd5aa98520066484eadd
geeky_python <shabs1505@gmail.com>
2022-03-27 19:23:05 +0530

    Bot code added

---
+++ bot.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const {Client, Intents, Message}  = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const token = process.env.BOT_TOKEN;
+
+const agents = ["Reyna", "Neon", "Raze", "Cypher", "Viper", "Brimstone", "Breach", "Omen", "Phoenix", "Jett", "Kayo", "Sova", "Sage", "Yoru", "Skye", "Astra", "Killjoy", "Chamber"];
+const roles = ["Duelist", "Sentinel", "Initiator", "Controller"];
+
+client.on('ready', () => {
+    console.log(client.user.username + " is online!");
+});
+
+client.on('messageCreate', (msg) => {
+
+    if(msg.channel.id === process.env.CHANNEL)
+    {
+        if(msg.content === "gen#")
+        {
+            console.log(`Message Author: ${msg.author.username} \nMessage Content: ${msg.content}`);
+            msg.reply(`@${msg.author.username}, your agent is ` + agents[Math.floor(Math.random() * agents.length)] + " and your role is " + roles[Math.floor(Math.random() * roles.length)]);
+        }
+
+        else if(msg.content === "help#")
+        {
+            msg.reply("To generate a random agent, type gen#");
+        }
+    }
+});
+
+client.login(token);
