97ed9cf84af69a086cf2edf8223334e7ccca2300
GeekyPython <shabs1505@gmail.com>
2022-08-05 17:09:12 +0530

    Weapon update for the randomizer bot

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ const token = process.env.BOT_TOKEN;
 
 const agents = ["Reyna", "Neon", "Raze", "Cypher", "Viper", "Fade", "Brimstone", "Breach", "Omen", "Phoenix", "Jett", "Kayo", "Sova", "Sage", "Yoru", "Skye", "Astra", "Killjoy", "Chamber"];
 const roles = ["Duelist", "Sentinel", "Initiator", "Controller"];
-const primary_weapons = ["Odin, Ares, Marshal", "Operator", "Vandal", "Stinger", "Spectre", "Phantom"];
+const primary_weapons = ["Odin, Ares, Marshal", "Operator", "Vandal", "Stinger", "Spectre", "Phantom", "Bulldog", "Guardian", "Bucky", "Judge"];
+const secondary_weapons = ["Classic", "Ghost", "Frenzy", "Shorty", "Sheriff"];
 
 client.on('ready', () => {
     console.log(client.user.username + " is online!");
@@ -22,6 +23,11 @@ client.on('messageCreate', (msg) => {
         msg.reply(`@${msg.author.username}, your agent is ` + agents[Math.floor(Math.random() * agents.length)] + " and your role is " + roles[Math.floor(Math.random() * roles.length)]);
     }
 
+    if(msg.content === "wep#")
+    {
+        console.log(`Message Author: ${msg.author.username} \nMessage Content: ${msg.content}`);
+        msg.reply(`@${msg.author.username}, your primary weapon is ` + primary_weapons[Math.floor(Math.random() * primary_weapons.length)] + " and your sidearm is " + secondary_weapons[Math.floor(Math.random() * secondary_weapons.length)]);
+    }
 
     else if(msg.content === "help#")
     {
